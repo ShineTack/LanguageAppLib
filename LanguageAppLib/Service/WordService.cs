96c4b8639304cf19edf81e109a69baf571b2e866
Paul Litvinov <tackshine@gmail.com>
2021-03-26 21:49:20 +0200

    обновление сервиса

--- LanguageAppLib/Service/WordService.cs
+++ LanguageAppLib/Service/WordService.cs
@@ -19,16 +19,18 @@ namespace LanguageAppLib.Service
         /// </summary>
         /// <param name="languageFrom">Язык с которого переводим</param>
         /// <param name="languageTo">Язык на который переводим</param>
-        /// <returns>Возвращает перечесление слов через yield для связывания с кнопками через foreach</returns>
-        public IEnumerable<Word> GetFourWords(Language languageFrom, Language languageTo)
+        /// <returns>Возвращает список слов</returns>
+        public List<Word> GetFourWords(Language languageFrom, Language languageTo)
         {
             List<Word> words = (from word in _dbContext.Words
                                 where languageFrom.Id == word.TextLanguage.Id && languageTo.Id == word.TranslationLanguage.Id
                                 select word).ToList();
+            List<Word> result = new List<Word>();
             for(int i =0; i < 4;i++)
             {
-                yield return words[new Random(Guid.NewGuid().GetHashCode()).Next(words.Count)];
+                result.Add(words[new Random(Guid.NewGuid().GetHashCode()).Next(words.Count)]);
             }
+            return result;
         }
     }
 }
